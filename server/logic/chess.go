a0c72e6550f365569cfc0e1242a3a65cd7ddd6bc
KoanJan <j.kehong@gmail.com>
2017-08-16 16:01:52 +0800

    feat: 下子时添加判断以及其他操作
    1. 比赛是否已经结束;
    2. 是否下子之后会被将军;
    3. 更新下子记录.

--- server/logic/chess.go
+++ server/logic/chess.go
@@ -7,6 +7,7 @@ import (
 	"ChineseChess/server/cache"
 	"ChineseChess/server/daf"
 	"ChineseChess/server/models"
+	"gopkg.in/mgo.v2/bson"
 )
 
 // Play 下子
@@ -15,14 +16,42 @@ func Play(x1, y1, x2, y2 int32, boardID, userID string) error {
 	// 验证是否符合规则
 	return cache.UpdateBoardCache(boardID, func(board *models.ChessBoard) error {
 
+		if board.WinnerID != nil {
+			return errors.New("比赛已经结束")
+		}
+
 		if !AllowedUnderRules(x1, y1, x2, y2, board, userID) {
 			return errors.New("不符合游戏规则")
 		}
+
+		// 保存执行前的参数
+		var cacheV1, cacheV2 int32 = board.Get(x1, y1), board.Get(x2, y2)
+
+		// 执行
 		board.Set(x2, y2, board.Get(x1, y1))
 		board.Set(x1, y1, models.PieceNo)
+
+		if cacheV2 == models.PieceJiang || cacheV2 == models.PieceShuai {
+
+			// 吃将
+			winnerID := bson.ObjectIdHex(userID)
+			board.WinnerID = &winnerID
+		} else if IsInDanger(board, userID) {
+
+			// 如果走完这步会被吃将,则撤销本次操作
+			board.Set(x1, y1, cacheV1)
+			board.Set(x2, y2, cacheV2)
+			return errors.New("正在被将军!")
+		}
+
+		// 更新下子记录
+		board.Steps = append(board.Steps, models.Step{x1, y1, x2, y2})
+
+		// 更新到数据库
 		if err := daf.Update(board); err != nil {
 			fmt.Errorf("更新到数据库失败: %v\n", err)
 		}
+
 		return nil
 	})
 }
